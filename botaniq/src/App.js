abec0c1ee7ee6a8db95f3400d2e70451aafecc21
Dilahara MMS <41099408+chapayev0@users.noreply.github.com>
2024-01-22 19:44:08 +0530

    initial commit

---
+++ botaniq/src/App.js
@@ -0,0 +1,14 @@
+import { PrimeReactProvider } from 'primereact/api';
+
+import "primereact/resources/themes/lara-light-teal/theme.css";
+
+import { Button } from 'primereact/button';
+        
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        
+           <Button label="Submit" />
+       
+    );
+}
